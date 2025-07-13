c91e8c75c6e256221665946c767aa25d818a2628
Sumanth Neela <sumanthneela@mac.lan>
2025-07-13 12:51:03 -0400

    sum

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,15 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  // plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/', // Fixed: Use root path for GitHub Pages main site deployment
+  base: '/resume-website/', // ✅ Important for GitHub Pages
+  plugins: [react()],
+  // base: '/', // Fixed: Use root path for GitHub Pages main site deployment
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
